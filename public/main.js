04a7b42a18aa0234736d7ff9b95935e2d8ed9daa
Nhatlinh101019999 <luunhatlinh101019999@gmail.com>
2023-05-10 14:31:13 +0700

    done

---
+++ public/main.js
@@ -0,0 +1,8 @@
+// Menu
+var menu = document.querySelector("#menu");
+var menuButton = document.querySelector("#menu-button");
+
+function myFunction(x) {
+  menu.classList.toggle("invisible");
+  x.classList.toggle('fa-xmark');
+}
